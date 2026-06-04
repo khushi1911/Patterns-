473eae878e487732d5bce036ad9494ca8aa43aff
khushi1911 <khushikaushik2030@gmail.com>
2026-06-04 18:29:45 +0530

    4th

---
+++ inv_half_pyra.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class inv_half_pyra {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("enter rows :");
+        int n = sc.nextInt();
+        
+
+        for (int i=n ; i>=1 ; i--){
+            for (int j=1 ; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
