00cade57f7eaffe256a9bc7dbe202c5c8147591f
khushi1911 <khushikaushik2030@gmail.com>
2026-06-04 18:17:36 +0530

    1st

---
+++ solid_rectangle.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class solid_rectangle {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("enter rows :");
+        int n = sc.nextInt();
+        System.out.println("enter columns :");
+        int m = sc.nextInt();
+
+        for (int i=1 ; i<=n ; i++){
+            for (int j=1 ; j<=m ; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
