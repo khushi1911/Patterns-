e0f35e3316fed4d06f7c7b819bdfa417f474524b
khushi1911 <khushikaushik2030@gmail.com>
2026-06-04 18:54:20 +0530

    9th

---
+++ tri_binary.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class tri_binary{
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("enter rows :");
+        int n = sc.nextInt();
+        
+
+        for (int i=1 ; i<=n ; i++){
+            for (int j=1 ; j<=i ; j++){
+                int sum =i+j;
+                if(sum%2 ==0){
+                    System.out.print("1");
+                }else{
+                    System.out.print("0");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
